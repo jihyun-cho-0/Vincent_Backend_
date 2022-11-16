db7de91f6e378ac520f967fd896077452c598f38
Carrotww <rjgjfl90@naver.com>
2022-11-16 15:31:55 +0900

    model post, comment, filterimage

--- filter/models.py
+++ filter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class FilterImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='filter_user')
+    filter_name = models.CharField(max_length=25)
+    filter_image = models.ImageField(blank=False, upload_to='filter/%Y/%m/')
+
+    def __str__(self):
+        return str(self.filter_name)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    post_image = models.ImageField(blank=True, upload_to='%Y/%m/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_user")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment_post")
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name="comment_likes", blank=True)
+    
+    def __str__(self):
+        return str(self.comment)
